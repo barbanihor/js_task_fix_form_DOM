0ce2eff909d38da4ee9229ee0152f221d7de5d33
Ihor Barban <ihorbarban@Ihors-MacBook-Air.local>
2025-01-24 16:24:50 +0100

    Not solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const listOfInputs = Array.from(document.querySelectorAll('input'));
+
+listOfInputs.forEach((elemInput) => {
+  function prepareAttribute(nameAttr) {
+    return nameAttr.charAt(0).toUpperCase() + nameAttr.slice(1);
+  }
+
+  const inputName = elemInput.getAttribute('name');
+  const parentNode = elemInput.parentElement;
+  const label = document.createElement('label');
+
+  elemInput.setAttribute('placeholder', prepareAttribute(inputName));
+
+  parentNode.insertBefore(label, elemInput);
+  label.appendChild(elemInput);
+});
